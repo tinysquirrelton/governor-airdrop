b1be7b3f8269a3fa6db4b6c2652652bb3d892e6e
tinysquirrelton <soheil.saadati@hotmail.com>
2021-01-16 22:57:37 +0100

    footer, containers, clean up and constants

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
-import { store } from "./data/store";
 import Routes from "./routes";
 import { ToastContainer } from "react-toastify";
 import { X } from "react-feather";
@@ -10,7 +8,7 @@ const Close = ({ closeToast }) => <X size={20} onClick={closeToast} />;
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <>
         <ToastContainer
           position={"bottom-right"}
           autoClose={3000}
@@ -20,7 +18,7 @@ export default class App extends Component {
           draggablePercent={25}
         />
         <Routes />
-      </Provider>
+      </>
     );
   }
 }
