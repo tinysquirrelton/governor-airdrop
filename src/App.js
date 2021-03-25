b0705b5d3f41019f45bf8f0287d95d77da08b655
tinysquirrelton <soheil.saadati@hotmail.com>
2021-03-25 18:25:49 +0100

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Routes from "./routes";
-import { toast } from "react-toastify";
 import { ToastContainer } from "react-toastify";
 import { X } from "react-feather";
 
@@ -9,29 +8,7 @@ const Close = ({ closeToast }) => <X size={20} onClick={closeToast} />;
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.merkle = merkle;
-    this.GDAOABI = GDAOABI;
-    this.GDAOAddress = GDAOAddress;
   }
-
-  async componentDidMount() {
-    this.connectWeb3();
-  }
-
-  // onConnect = () => {
-  //   this.GDAOContract = new web3.eth.Contract(this.GDAOABI, this.GDAOAddress);
-  //   this.airdropContract = new web3.eth.Contract(
-  //     this.merkle.contractABI,
-  //     this.merkle.contractAddress
-  //   );
-  // };
-
-  // onResetConnect = () => {
-  //   this.setState({
-  //     isAirdropClaimed: false,
-  //     isEligible: false,
-  //   });
-  // };
 
   render() {
     return (
